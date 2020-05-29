60d04ff460653971dfd02409a7d08d864c983f82
nanarino <kogawananari@gmail.com>
2020-05-29 15:27:55 +0800

    FIX    ticket优化

--- eleme_reapi/computed/ticket.py
+++ eleme_reapi/computed/ticket.py
@@ -1,15 +1,9 @@
-import random
-
-
-def _random_hex(n):
-    """生成n位长度的随机十六进制字符串"""
-    return hex(random.randint(0, 16**n - 1))[2:].zfill(n)
+import uuid
 
 
 def ticket():
     """生成请求饿百接口所需的ticket参数"""
-    ticket_arr = [_random_hex(i) for i in (8, 4, 4, 4, 12)]
-    return '-'.join(ticket_arr).upper()
+    return str(uuid.uuid1()).upper()
 
 
 if __name__ == "__main__":
